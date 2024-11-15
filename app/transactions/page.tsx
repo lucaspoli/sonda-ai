211ee92e6fa759df738e7cb6aa322b5fe7af0ccd
Lucas Ditchun <lucas.ditchun@gmail.com>
2024-11-15 19:04:20 -0300

    feat: add modal for creating new transactions

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,19 +1,16 @@
 import { db } from "@/app/_lib/prisma";
-import { Button } from "../_components/ui/button";
-import { ArrowDownUpIcon } from "lucide-react";
 import { DataTable } from "./_components/data-table";
 import { transactionColumns } from "./_components/columns";
+import AddTransactionsButton from "../_components/add-transaction-button";
+import PageTitle from "../_components/page-title";
 
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
   return (
     <div className="space-y-4 p-8">
       <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-bold">Transactions</h1>
-        <Button className="rounded-full px-6 text-sm">
-          <ArrowDownUpIcon />
-          Add Transaction
-        </Button>
+        <PageTitle>Transactions</PageTitle>
+        <AddTransactionsButton />
       </div>
       <div>
         <DataTable columns={transactionColumns} data={transactions} />

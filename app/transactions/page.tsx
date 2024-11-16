911693ad39255e4f0a7de34d6b7394c6fd7822a5
Lucas Ditchun <lucas.ditchun@gmail.com>
2024-11-16 16:15:25 -0300

    feat: add navbar to transactions page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,19 +3,23 @@ import { DataTable } from "./_components/data-table";
 import { transactionColumns } from "./_components/columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import PageTitle from "../_components/page-title";
+import Navbar from "../_components/navbar";
 
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
   return (
-    <div className="space-y-4 p-8">
-      <div className="flex w-full items-center justify-between">
-        <PageTitle>Transactions</PageTitle>
-        <AddTransactionButton />
+    <>
+      <Navbar />
+      <div className="space-y-4 p-8">
+        <div className="flex w-full items-center justify-between">
+          <PageTitle>Transactions</PageTitle>
+          <AddTransactionButton />
+        </div>
+        <div>
+          <DataTable columns={transactionColumns} data={transactions} />
+        </div>
       </div>
-      <div>
-        <DataTable columns={transactionColumns} data={transactions} />
-      </div>
-    </div>
+    </>
   );
 };
 

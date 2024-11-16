798f32bea333a4772faf9f069c072fa0b6837a33
Lucas Ditchun <lucas.ditchun@gmail.com>
2024-11-16 16:33:12 -0300

    feat: add subscription route

---
+++ app/subscription/page.tsx
@@ -0,0 +1,14 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Navbar from "../_components/navbar";
+
+const SubscriptionPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return <Navbar />;
+};
+
+export default SubscriptionPage;

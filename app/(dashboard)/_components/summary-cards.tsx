640c512c1b90fcba41393fc14fffae7ed2360523
Lucas Ditchun <lucas.ditchun@gmail.com>
2024-11-17 11:25:41 -0300

    feat: add SummaryCards component to render financial summaries

---
+++ app/(dashboard)/_components/summary-cards.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+import { Card, CardContent, CardHeader } from "../../_components/ui/card";
+import AddTransactionButton from "../../_components/add-transaction-button";
+
+interface SummaryCardProps {
+  icon: ReactNode;
+  title: string;
+  amount: number;
+  size?: "small" | "large";
+}
+
+const SummaryCards = ({ icon, title, amount, size }: SummaryCardProps) => {
+  return (
+    <div className="space-y-6">
+      <Card>
+        <CardHeader className="flex-row items-center gap-4">
+          {icon}
+          <p
+            className={`text-white ${size === "small" ? "opacity-70" : "text-lg opacity-80"}`}
+          >
+            {title}
+          </p>
+        </CardHeader>
+        <CardContent className="flex justify-between">
+          <span
+            className={`font-bold ${size === "small" ? "text-2xl" : "text-4xl"}`}
+          >
+            {Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+            }).format(amount)}
+          </span>
+          {size === "large" && <AddTransactionButton />}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SummaryCards;

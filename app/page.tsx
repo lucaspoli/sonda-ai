b5ed51c000f42ee234d4f0c6829a220d9b92e96e
Lucas Ditchun <lucas.ditchun@gmail.com>
2024-11-17 11:35:34 -0300

    feat: integrate Dashboard component into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import Navbar from "./_components/navbar";
 import { redirect } from "next/navigation";
+import DashboardPage from "./(dashboard)/dashboard";
 
 const HomePage = async () => {
   const { userId } = await auth();
@@ -8,7 +9,12 @@ const HomePage = async () => {
     redirect("/login");
   }
 
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <DashboardPage />
+    </>
+  );
 };
 
 export default HomePage;
